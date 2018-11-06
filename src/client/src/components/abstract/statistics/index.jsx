6ff32d2c810d2e6a3cbfcf9ec95e9884d182ccab
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-06 13:20:14 +0200

    make statistics component

---
+++ src/client/src/components/abstract/statistics/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export default class Statistics extends Component {
+  render() {
+    const { number, value } = this.props;
+    console.log(number)
+    return (
+      <div className='statistics'>
+        <h2>{number}</h2>
+        <h3>{value}</h3>
+      </div>
+    )
+  }
+}
