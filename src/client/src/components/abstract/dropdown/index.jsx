49fafad44f7de60dc2b3cfd72e886c6a4be98f6d
Kanana <knanahassouna2@gmail.com>
2018-11-06 16:01:53 +0200

    add proptypes and error handling

--- src/client/src/components/abstract/dropdown/index.jsx
+++ src/client/src/components/abstract/dropdown/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import propType from 'prop-types';
 import './style.css'
 
 export default class Dropdown extends Component {
@@ -8,12 +9,21 @@ export default class Dropdown extends Component {
       <React.Fragment>
           <label htmlFor={name} className='label-field'>{label}
             <select name={name} className='drop-field' style={ { width: width } }>
-                {options.map((option,index) => (
+                
+                { options ?( options.map((option,index) => (
                 <option value={option} key={index}>{option}</option>
-                ))}
+                ))) : ""
+                }
             </select>
           </label>
       </React.Fragment>
     )
   }
+}
+
+Dropdown.propType = {
+  name: propType.string.isRequired,
+  label: propType.string.isRequired, 
+  options: propType.array.isRequired, 
+  width : propType.string.isRequired
 }
