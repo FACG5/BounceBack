7e3add1d9d5d42918fdcc726cbe957f53e4becd7
Kanana <knanahassouna2@gmail.com>
2018-11-06 14:56:02 +0200

    add proptypes

--- src/client/src/components/abstract/input/index.jsx
+++ src/client/src/components/abstract/input/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import './index.css';
 
 export default function index(props) {
@@ -6,17 +7,26 @@ export default function index(props) {
   return (
     <div>
         <label htmlFor={name} className="label-field">
-        {label} :
-        <input 
-        name={ name }
-        type={ type } 
-        style={ { width: width } }
-        placeholder = {`type ${label} ...`}
-        className="input-field"
-        value={ value }
-        onChange={onChange}
-        />
+            {label} :
+            <input 
+                name={ name }
+                type={ type } 
+                style={ { width: width } }
+                placeholder = {`type ${label} ...`}
+                className="input-field"
+                value={ value }
+                onChange={onChange}
+            />
         </label>
     </div>
   )
 }
+index.propTypes = {
+    value: propTypes.string.isRequired,
+    onChange: propTypes.func.isRequired,
+    name: propTypes.string.isRequired,
+    label: propTypes.string,
+    type: propTypes.string,
+    width: propTypes.string,
+}
+
