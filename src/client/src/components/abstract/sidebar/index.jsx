91cfa1d44ad8f448ef2ea331c33b2bdb16c90727
asalaKM <asalamatarya@gmail.com>
2018-11-07 04:25:49 +0200

    create sidebr component thats include header titles links Relates #4

---
+++ src/client/src/components/abstract/sidebar/index.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import './style.css';
+import SideHeader from './sidebarHeader'
+import SideTitles from './sidebarTitles'
+import SideLinks from './sidebarLinks'
+
+class Sidebar extends Component{
+    render() {
+      return(
+          <div className="side-bar">
+                 <SideHeader />
+                 <SideTitles />
+                 <SideLinks />
+          </div>
+      )
+    }
+  }
+
+  export default Sidebar;
