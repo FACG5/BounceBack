71873cd6a0172c9a90095660780446cf2d58211d
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-07 16:00:10 +0200

    delete the buttons

--- src/client/src/components/pages/viewCourses/index.jsx
+++ src/client/src/components/pages/viewCourses/index.jsx
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import Header from '../../abstract/header';
 import Table from '../../abstract/Table';
-import Footer from '../../abstract/footer'
-import Button from '../../abstract/button'
+import Footer from '../../abstract/footer';
 
 export default class Courses extends Component {
-  linking = () => {
-    this.props.history.push('/courses/add')
-  }
   render() {
     return (
       <React.Fragment>
@@ -21,17 +17,6 @@ export default class Courses extends Component {
         />
         <Table 
         rows={[['Course Name', 'Course Id', 'start', 'end', 'Action'], ['Painting', '4', '04/05/2017', '04/05/2017', <React.Fragment><i className="fas fa-trash-alt"></i><i className="fas fa-info-circle"></i></React.Fragment>], ['Painting', '4', '04/05/2017', '04/05/2017', <React.Fragment><i className="fas fa-trash-alt"></i><i className="fas fa-info-circle"></i></React.Fragment>], ['Painting', '4', '04/05/2017', '04/05/2017', <React.Fragment><i className="fas fa-trash-alt"></i><i className="fas fa-info-circle"></i></React.Fragment>], ['Painting', '4', '04/05/2017', '04/05/2017', <React.Fragment><i className="fas fa-trash-alt"></i><i className="fas fa-info-circle"></i></React.Fragment>]]}
-        />
-        <Button 
-        value='Edit & save'
-        />
-        <Button 
-        value='Back'
-        color='red'
-        />
-        <Button 
-        value='Add Course'
-        onClick={this.linking }
         />
         <Footer />
       </React.Fragment>
