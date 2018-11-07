e8951b75f482424c6ecab80db9bdc58a429070fc
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-07 10:22:02 +0200

    make the structure of app.js

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -1,10 +1,23 @@
-import React, { Component } from ' react '; 
+import React, { Component } from 'react';
+import Sidebar from '../components/abstract/sidebar';
+import Container from '../components/abstract/layout/Container';
+import './App.css'
 
 class App extends Component {
+  state = {
+    login: true
+  }
   render() {
     return (
       <div className="App">
-    
+      {!this.state.login ? <h1>login page</h1> : (
+        <React.Fragment>
+        <Sidebar /> 
+        <Container>
+        {/* Content here */}
+        </Container>
+        </React.Fragment>
+      )}
       </div>
     );
   }
