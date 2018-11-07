5dcb7f02dd4129b33186fed8d88430c7e0712d67
asalaKM <asalamatarya@gmail.com>
2018-11-07 04:22:39 +0200

    create sidebar header component with css style

---
+++ src/client/src/components/abstract/sidebar/sidebarHeader/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './style.css';
+
+class SideHeader extends Component{
+    render() {
+      return(
+          <div className="side-header">
+            <img className="logo" src="http://www8.0zz0.com/2018/11/07/05/580748308.png" alt="logo"/>
+          </div>
+      )
+    }
+  }
+  
+  export default SideHeader;
