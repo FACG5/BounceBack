a4f0e534fcf3a65b17fe56d8fe5c269ff0cea2d2
asalaKM <asalamatarya@gmail.com>
2018-11-07 09:51:17 +0200

    edit app.js

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import React, { Component } from 'react'; 
  
-=======
-import React, { Component } from ' react '; 
-
->>>>>>> e420c85794da91a415569afb19bb5b028aac3ebc
 class App extends Component {
   render() {
     return (
