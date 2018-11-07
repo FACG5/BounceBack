333532db83d569b9d90f8e2c5e62a25323db8b5b
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-07 11:17:42 +0200

    the main structure on App.js

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -1,24 +1,32 @@
-import React, { Component } from 'react';
-import Sidebar from '../components/abstract/sidebar';
-import Container from '../components/abstract/layout/Container';
-import './App.css'
+import React, { Component } from "react";
+import Sidebar from "../components/abstract/sidebar";
+import Container from "../components/abstract/layout/Container";
+import ViewParticipants from "../components/pages/viewParticipants";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
   state = {
     login: true
-  }
+  };
   render() {
     return (
-      <div className="App">
-      {!this.state.login ? <h1>login page</h1> : (
-        <React.Fragment>
-        <Sidebar /> 
-        <Container>
-        {/* Content here */}
-        </Container>
-        </React.Fragment>
-      )}
-      </div>
+      <Router>
+        <div className="App">
+          {!this.state.login ? (
+            <h1>login page</h1> // Login Page
+            ) : (
+            <React.Fragment>
+              <Sidebar />
+              <Container>
+                <Switch>
+                  <Route exact path="/participants/view" component={ViewParticipants}/>  
+                </Switch>
+              </Container>
+            </React.Fragment>
+          ) }
+        </div>
+      </Router>
     );
   }
 }
