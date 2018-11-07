6df459f2195e6cced3eeba1e507692cb3529e68d
asalaKM <asalamatarya@gmail.com>
2018-11-07 04:23:32 +0200

    create sidebar links component with css style

---
+++ src/client/src/components/abstract/sidebar/sidebarLinks/index.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import {BrowserRouter as  Router} from 'react-router-dom';
+import './style.css';
+
+class SideLinks extends Component{
+    render() {
+      return(
+            <React.Fragment>
+              <Router>
+                <Link className="links" to="/report">report</Link>
+              </Router>
+              <Router>
+                <Link className="links" to="/login">logout</Link>
+              </Router>
+            </React.Fragment>
+      )
+    }
+  }
+  
+  export default SideLinks;
