e5ec767f570bd0bd27f8c091d5ff0d2d7a99adb0
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-07 12:55:51 +0200

    delete idenetation

--- src/client/src/components/abstract/Table/index.jsx
+++ src/client/src/components/abstract/Table/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Container from '../layout/Container'
+import Container from "../layout/Container";
 import "./style.css";
 
 export default class Table extends Component {
@@ -7,23 +7,23 @@ export default class Table extends Component {
     const { rows } = this.props;
     if (!rows) {
       return (
-      <Container>
-      <div className="table-error"> No Result</div>
-      </Container>
+        <Container>
+          <div className="table-error"> No Result</div>
+        </Container>
       );
     }
 
     return (
       <div>
-          <div className="table">
-            {rows.map((row, index) => (
-              <div key={index}>
-                {row.map((value, index) => (
-                  <span key={index}>{value}</span>
-                ))}
-              </div> 
-            ))}
-          </div>
+        <div className="table">
+          {rows.map((row, index) => (
+            <div key={index}>
+              {row.map((value, index) => (
+                <span key={index}>{value}</span>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
