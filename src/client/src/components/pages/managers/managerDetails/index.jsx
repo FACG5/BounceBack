3a50511737f06059cf681640810567fb049183c9
asalaKM <asalamatarya@gmail.com>
2018-11-09 23:53:37 +0200

    add manager details page layout #35

---
+++ src/client/src/components/pages/managers/managerDetails/index.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import {
+  state as initialState,
+  fields as fieldSet,
+  validationForm
+} from "./staticData";
+import Form from "./../../../abstract/Form";
+import Footer from '../../../abstract/footer';
+
+export default class index extends Component {
+  state = initialState;
+
+  onChange = event => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  goBack = event => {
+    this.props.history.push('/managers/view')
+  };
+
+  // the implemention waiting  back end api
+  onSubmit = event => {
+    event.preventDefault();
+    const fields = { ...this.state };
+    const error = validationForm(fields);
+    if (error) return this.setState({ error });
+
+    for (const key in fields) {
+      fields[key] = "";
+    }
+    this.setState(fields);
+  };
+
+  render() {
+    return (
+      <div>
+        <Form
+          title="Manager Details"
+          fields={fieldSet}
+          values={this.state}
+          onChange={this.onChange}
+          btnEvents={[this.onSubmit, this.goBack]}
+        />
+        <Footer />
+      </div>
+    );
+  }
+}
---
+++ src/client/src/components/pages/managers/managerDetails/staticData.js
@@ -0,0 +1,140 @@
+export const state = {
+    surname: "mohanned",
+    forename: "al-hanafi",
+    username: "mohannadhanafi",
+    date: "23/2/1995",
+    gender: "Male",
+    matrialStatus: "Single",
+    email: "mohannad@gmail.com",
+    address: "khan-younis",
+    postcode: "00970",
+    mobile: "0591234567",
+    landline: "082134569",
+    password: "123123",
+    confirmPassword: "123123"
+};
+export const fields = [
+    [{
+            tag: "Input",
+            label: "Surname",
+            name: "surname",
+            width: "15rem",
+            placeholder: "surname"
+        },
+        {
+            tag: "Input",
+            label: "Forename",
+            name: "forename",
+            width: "15rem",
+            placeholder: "forename"
+        },
+        {
+            tag: "Input",
+            label: "Username",
+            name: "username",
+            width: "15rem",
+            placeholder: "type username"
+        }
+    ],
+    [
+        {
+            tag: "Input",
+            label: "Day Of Birth",
+            name: "date",
+            width: "15rem",
+            type: "date",
+        },
+        {
+            tag: "DropDown",
+            label: "Gender",
+            name: "gender",
+            width: "15rem",
+            options: ["Male", "Female"]
+        },
+        {
+            tag: "DropDown",
+            label: "Martial Status",
+            name: "status",
+            width: "15rem",
+            options: ["single", "married"]
+        }
+    ],
+    [
+        {
+            tag: "Input",
+            label: "Email Adress",
+            name: "email",
+            width: "35rem",
+            placeholder: "type email adress"
+        }
+    ],
+    [
+        {
+            tag: "Input",
+            label: "Full Address",
+            name: "address",
+            width: "35rem",
+            placeholder: "type full adress"
+        },
+        {
+            tag: "Input",
+            label: "postcode",
+            name: "postcode",
+            width: "15rem",
+            placeholder: "postcode"
+        }
+    ],
+    [
+        {
+            tag: "Input",
+            label: "Mobile Number",
+            name: "Mobile",
+            width: "20rem",
+            placeholder: "Mobile Number"
+        },
+        {
+            tag: "Input",
+            label: "Land line",
+            name: "landline",
+            width: "20rem",
+            placeholder: "Land Line"
+        }
+    ],
+    [
+        {
+            tag: "Input",
+            label: "Password",
+            placeholder:'password',
+            name: "password",
+            width: "20rem",
+            type: "password",
+        },
+        {
+            tag: "Input",
+            label: "Confirm Password",
+            placeholder: "password",
+            name: "confirmPassword",
+            width: "20rem",
+            type: "password",
+        }
+    ],
+    [
+        {
+            tag: "Button",
+            value: "Edit & Save",
+            color: "#272727",
+        },
+        {
+            tag: 'Button',
+            value: "Back",
+            color: "#FF4800",
+        }
+    ]
+];
+
+export const validationForm = (fields) => {
+    for (const key in fields) {
+        if (fields[key] === "")
+            return `Please Check ${key}`;
+    }
+}
