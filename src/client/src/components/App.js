59e9b1cc48cb7fa8bbaa35004964c622f27437ca
asalaKM <asalamatarya@gmail.com>
2018-11-09 20:45:40 +0200

    add new rout #56

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -15,6 +15,7 @@ import ReportResult from './pages/reportResult'
 import Report from './pages/report'
 import addWorker from './pages/workers/addWorkers';
 import ParticipantCourseDetails from '../components/pages/participants/participantCourseDetails';
+import ParticipantCourseAdd from '../components/pages/participants/addParticipantCourse';
 import AddParticipantDate from '../components/pages/participants/addParticipantDate';
 import ParticipantDateDetails from '../components/pages/participants/participantDateDetails';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -49,6 +50,7 @@ class App extends Component {
                   <Route exact path="/managers/add" component={addManager} />
                   <Route exact path="/courses/add" component={addCourse} />
                   <Route exact path="/participants/course/details" component={ParticipantCourseDetails}/>
+                  <Route exact path="/participants/course/add" component={ParticipantCourseAdd}/>
                   <Route exact path="/participants/date/add" component={AddParticipantDate}/>
                   <Route exact path="/participants/date/details" component={ParticipantDateDetails}/>
                 </Switch>
