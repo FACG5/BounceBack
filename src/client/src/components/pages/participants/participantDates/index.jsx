a68ef4b861b2cd0c48a4bc4c8e737fca23a3250b
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-09 21:38:02 +0200

    make the dates page

--- src/client/src/components/pages/participants/participantDates/index.jsx
+++ src/client/src/components/pages/participants/participantDates/index.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../../abstract/header';
-import Footer from '../../../abstract/footer'
+import Table from '../../../abstract/Table';
+import Footer from '../../../abstract/footer';
+import Button from '../../../abstract/button';
 
 export default class Date extends Component {
     state = {
-        search: ""
+        search: "",
+        rows: [
+          ["Date name", "Date Id", "Start", "End", "Action"],
+          ["Date Title", "9868686", "15/5/2017", "15/12/2017", <React.Fragment><i className="fas fa-trash-alt"></i><Link to="/courses/details">
+        <i className="fas fa-info-circle"></i></Link></React.Fragment>],
+          ["Date Title", "9868686", "15/5/2017", "15/12/2017", <React.Fragment><i className="fas fa-trash-alt"></i><Link to="/courses/details">
+        <i className="fas fa-info-circle"></i></Link></React.Fragment>],
+          ["Date Title", "9868686", "15/5/2017", "15/12/2017", <React.Fragment><i className="fas fa-trash-alt"></i><Link to="/courses/details">
+        <i className="fas fa-info-circle"></i></Link></React.Fragment>],
+          ["Date Title", "9868686", "15/5/2017", "15/12/2017", <React.Fragment><i className="fas fa-trash-alt"></i><Link to="/courses/details">
+        <i className="fas fa-info-circle"></i></Link></React.Fragment>]
+        ]
       };
       onChange = event => {
         const { value, name } = event.target;
@@ -14,6 +28,9 @@ export default class Date extends Component {
     return (
       <React.Fragment>
         <Header value='View Dates' />
+        <Table rows = {this.state.rows} />
+        <Button value= 'Edit & save' />
+        <Button value= 'Back' color='red' />
         <Footer />
       </React.Fragment>
     )
