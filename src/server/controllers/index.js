c88278876a0881fb8086e3085054e086b2f98305
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-14 19:19:59 +0200

    make the server route of get Courses

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const getCount = require('./getCount');
+const courses = require('./courses');
 
 const Router = express.Router();
 
 Router.get('/overview', getCount.get);
+Router.get('/courses', courses.get);
 
 module.exports = Router;
