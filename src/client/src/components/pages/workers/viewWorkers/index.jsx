6ccbe3a4deded39c1623704f2e8d0e5f17a81e18
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-14 23:19:09 +0200

    make the request to delete worker

--- src/client/src/components/pages/workers/viewWorkers/index.jsx
+++ src/client/src/components/pages/workers/viewWorkers/index.jsx
@@ -15,7 +15,23 @@ export default class ViewWorkers extends Component {
     const { value, name } = event.target;
     this.setState({ [name]: value });
   };
-  componentDidMount = async () => {
+
+  deleteWorker = id => {
+    if (window.confirm("Are you sure that you want to delete this worker ?")) {
+      axios("/workers", {
+        method: "DELETE",
+        data: {
+          workerId: id
+        }
+      }).then(result => {
+        this.getData().then(() => {
+          alert(result.data.message);
+        });
+      });
+    }
+  };
+
+  getData = async () => {
     try {
       const data = await axios("/workers");
       const finalData = data.data.workersData;
@@ -23,16 +39,23 @@ export default class ViewWorkers extends Component {
       finalData.map(row =>
         array.push([
           row.username,
-          row.date_of_birth.split('T')[0],
+          row.date_of_birth.split("T")[0],
+          <>
+          <i className="fas fa-trash-alt" onClick={() => this.deleteWorker(row.id)} />
           <Link to="/worker/details">
             <i className="fas fa-info-circle" />
           </Link>
+          </>
         ])
       );
       this.setState({ rows: array });
     } catch (err) {
       console.log(err); // waiting for boundery error handling
     }
+  };
+
+  componentDidMount = () => {
+    this.getData();
   };
   render() {
     return (
