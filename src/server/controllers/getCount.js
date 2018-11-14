119419f959e916e906e5bc0a8bca065a81631a6d
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-14 11:06:53 +0200

    make the function of get Counts

---
+++ src/server/controllers/getCount.js
@@ -0,0 +1,10 @@
+const participant = require('../database/models/participant');
+const course = require('../database/models/course');
+const worker = require('../database/models/worker');
+
+exports.get = async (req, res) => {
+    const countParticipant = await participant.findAndCountAll();
+    const countCourse = await course.findAndCountAll();
+    const countWorker = await worker.findAndCountAll();
+    res.send({countParticipant, countCourse, countWorker});
+};
