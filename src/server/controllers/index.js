08c0214cf75719b438899f2a0ddb9246e0da319e
asalaKM <asalamatarya@gmail.com>
2018-11-14 20:40:48 +0200

    add new rout to fetch

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const getCount = require('./getCount');
+const participants = require('./participants/getParticipants');
 
 const Router = express.Router();
 
 Router.get('/overview', getCount.get);
+Router.get('/participants', participants.get);
 
 module.exports = Router;
