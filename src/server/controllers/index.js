96d13d05d18a09188a8b8703cb5b8e7027bffbc5
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-14 20:06:51 +0200

    make the function of the function

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const getCount = require('./getCount');
 const courses = require('./courses');
+const workers = require('./workers');
 
 const Router = express.Router();
 
 Router.get('/overview', getCount.get);
 Router.get('/courses', courses.get);
+Router.get('/workers', workers.get);
 
 module.exports = Router;
