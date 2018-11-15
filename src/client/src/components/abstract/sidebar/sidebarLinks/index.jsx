867e84affa508faeec0f46bc4db4e0f62d419919
Kanana <knanahassouna2@gmail.com>
2018-11-15 08:37:14 +0200

    handle logout link

--- src/client/src/components/abstract/sidebar/sidebarLinks/index.jsx
+++ src/client/src/components/abstract/sidebar/sidebarLinks/index.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
-import './style.css';
+import { Link } from "react-router-dom";
+import { Consumer } from "./../../../context";
+import "./style.css";
 
-class SideLinks extends Component{
-    render() {
-      return(
-            <React.Fragment>
-                <Link className="links" to="/report">report</Link>
-                <Link className="links" to="/login">logout</Link>
-            </React.Fragment>
-      )
-    }
+class SideLinks extends Component {
+  logout = dispatch => {
+    dispatch({ type: "LOGIN_USER", payload: { logging: false } });
+  };
+  render() {
+    return (
+      <Consumer>
+        {value => (
+          <React.Fragment>
+            <Link className="links" to="/report">
+              report
+            </Link>
+            <span className="links" onClick={() => this.logout(value.dispatch)}>
+              logout
+            </span>
+          </React.Fragment>
+        )}
+      </Consumer>
+    );
   }
-  
-  export default SideLinks;
-  
+}
+
+export default SideLinks;
