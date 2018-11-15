b236c35680eb2107a031b00a4168c1e98e4adee9
asalaKM <asalamatarya@gmail.com>
2018-11-15 13:39:04 +0200

    edit index.js

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,6 @@ const app = require("./app");
 const port = app.get("port");
 const { connection } = require("./database/models");
 
-connection.sync().then(() => {
+connection.sync({force:true}).then(() => {
   app.listen(port, () => console.log(`the server started on port ${port}`));
 });
