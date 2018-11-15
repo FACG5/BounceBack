f163ec305d85a8ac8a651426083f2b10b8fc2f24
Kanana <knanahassouna2@gmail.com>
2018-11-15 09:59:13 +0200

    remove cookie after logout

--- src/client/src/components/abstract/sidebar/sidebarLinks/index.jsx
+++ src/client/src/components/abstract/sidebar/sidebarLinks/index.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Consumer } from "./../../../context";
+import removeCookies from "./removeCookies";
 import "./style.css";
 
 class SideLinks extends Component {
   logout = dispatch => {
+    removeCookies();
     dispatch({ type: "LOGIN_USER", payload: { logging: false } });
   };
   render() {
