646e0e008d2b2483b423c1598ac42f080b5bc4b1
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-18 11:27:48 +0200

    add /api/v2 for router

--- src/server/app.js
+++ src/server/app.js
@@ -9,7 +9,7 @@ app.set("port", process.env.PORT || 5000);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(Router);
+app.use('/api/v2', Router);
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
