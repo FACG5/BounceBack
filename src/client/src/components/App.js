ab2717a7d20487ba927e2d8c99560b31525cc092
Kanana <knanahassouna2@gmail.com>
2018-11-19 10:44:06 +0200

    use HOC

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -25,7 +25,7 @@ import AddParticipantDate from "../components/pages/participants/addParticipantD
 import ParticipantDateDetails from "../components/pages/participants/participantDateDetails";
 import participantDetails from "./pages/participants/participantDetails";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Consumer } from "./context";
+import contextHoc from "./abstract/HOC/contextHoc";
 
 import "./App.css";
 
@@ -33,133 +33,115 @@ class App extends Component {
   state = {
     login: true
   };
+  componentWillMount = () => {
+    const { dispatch } = this.props.context;
+    const payload = JSON.parse(localStorage.getItem("user"));
+    dispatch({ type: "LOGIN_USER", payload });
+  };
   render() {
+    const { context } = this.props;
     return (
-      <Consumer>
-        {value => (
-          <Router>
-            <div className="App">
-              {!value.logging ? (
-                <Route component={Login} />
-              ) : (
-                <React.Fragment>
-                  <div className="view-app">
-                    <Sidebar />
-                    <Container>
-                      <Switch>
-                        <Route
-                          exact
-                          path="/participants/view"
-                          component={ViewParticipants}
-                        />
-                        <Route
-                          exact
-                          path="/courses/view"
-                          component={viewCourses}
-                        />
-                        <Route
-                          exact
-                          path="/managers/view"
-                          component={viewManagers}
-                        />
-                        <Route
-                          exact
-                          path="/participants/dates"
-                          component={participantDate}
-                        />
-                        <Route exact path="/" component={Dashboard} />
-                        <Route
-                          exact
-                          path="/participants/add"
-                          component={AddParticipant}
-                        />
-                        <Route
-                          exact
-                          path="/report/result"
-                          component={ReportResult}
-                        />
-                        <Route exact path="/report" component={Report} />
-                        <Route
-                          exact
-                          path="/workers/view"
-                          component={ViewWorker}
-                        />
-                        <Route
-                          exact
-                          path="/workers/add"
-                          component={addWorker}
-                        />
-                        <Route
-                          exact
-                          path="/managers/add"
-                          component={addManager}
-                        />
-                        <Route
-                          exact
-                          path="/courses/add"
-                          component={addCourse}
-                        />
-                        <Route
-                          exact
-                          path="/worker/details"
-                          component={workerDetails}
-                        />
-                        <Route
-                          exact
-                          path="/courses/details/:id"
-                          component={courseDetails}
-                        />
-                        <Route
-                          exact
-                          path="/manager/details"
-                          component={managerDetails}
-                        />
-                        <Route
-                          exact
-                          path="/participants/prison"
-                          component={prisonDetails}
-                        />
-                        <Route
-                          exact
-                          path="/participants/course/details"
-                          component={ParticipantCourseDetails}
-                        />
-                        <Route
-                          exact
-                          path="/participants/course/add"
-                          component={ParticipantCourseAdd}
-                        />
-                        <Route
-                          exact
-                          path="/participants/date/add"
-                          component={AddParticipantDate}
-                        />
-                        <Route
-                          exact
-                          path="/participants/date/details"
-                          component={ParticipantDateDetails}
-                        />
-                        <Route
-                          exact
-                          path="/participant/details"
-                          component={participantDetails}
-                        />
-                        <Route
-                          exact
-                          path="/participants/courses"
-                          component={participantCourse}
-                        />
-                      </Switch>
-                    </Container>
-                  </div>
-                </React.Fragment>
-              )}
-            </div>
-          </Router>
-        )}
-      </Consumer>
+      <Router>
+        <div className="App">
+          {!context.logging ? (
+            <Route component={Login} />
+          ) : (
+            <React.Fragment>
+              <div className="view-app">
+                <Sidebar />
+                <Container>
+                  <Switch>
+                    <Route
+                      exact
+                      path="/participants/view"
+                      component={ViewParticipants}
+                    />
+                    <Route exact path="/courses/view" component={viewCourses} />
+                    <Route
+                      exact
+                      path="/managers/view"
+                      component={viewManagers}
+                    />
+                    <Route
+                      exact
+                      path="/participants/dates"
+                      component={participantDate}
+                    />
+                    <Route exact path="/" component={Dashboard} />
+                    <Route
+                      exact
+                      path="/participants/add"
+                      component={AddParticipant}
+                    />
+                    <Route
+                      exact
+                      path="/report/result"
+                      component={ReportResult}
+                    />
+                    <Route exact path="/report" component={Report} />
+                    <Route exact path="/workers/view" component={ViewWorker} />
+                    <Route exact path="/workers/add" component={addWorker} />
+                    <Route exact path="/managers/add" component={addManager} />
+                    <Route exact path="/courses/add" component={addCourse} />
+                    <Route
+                      exact
+                      path="/worker/details"
+                      component={workerDetails}
+                    />
+                    <Route
+                      exact
+                      path="/courses/details/:id"
+                      component={courseDetails}
+                    />
+                    <Route
+                      exact
+                      path="/manager/details"
+                      component={managerDetails}
+                    />
+                    <Route
+                      exact
+                      path="/participants/prison"
+                      component={prisonDetails}
+                    />
+                    <Route
+                      exact
+                      path="/participants/course/details"
+                      component={ParticipantCourseDetails}
+                    />
+                    <Route
+                      exact
+                      path="/participants/course/add"
+                      component={ParticipantCourseAdd}
+                    />
+                    <Route
+                      exact
+                      path="/participants/date/add"
+                      component={AddParticipantDate}
+                    />
+                    <Route
+                      exact
+                      path="/participants/date/details"
+                      component={ParticipantDateDetails}
+                    />
+                    <Route
+                      exact
+                      path="/participant/details"
+                      component={participantDetails}
+                    />
+                    <Route
+                      exact
+                      path="/participants/courses"
+                      component={participantCourse}
+                    />
+                  </Switch>
+                </Container>
+              </div>
+            </React.Fragment>
+          )}
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default contextHoc(App);
