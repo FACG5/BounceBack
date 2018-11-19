4a6ce73d71fa4e87ce3de9ffa63705c7f552f9b4
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-19 12:03:10 +0200

    remove comments

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const dbConfig = env => {
-  // if (env === "development") {
+  if (env === "development") {
     return {
       username: process.env.db_username,
       password: process.env.db_password,
@@ -7,16 +7,16 @@ const dbConfig = env => {
       host: "localhost",
       dialect: "postgres"
     };
-  // } else if (env === "production") {
-  //   return {
-  //     database: process.env.DB_PRODUCTION_URL,
-  //     username: process.env.PRODUCTION_DB_USERNAME,
-  //     password: process.env.PRODUCTION_DB_PASSWORD,
-  //     host: process.env.PRODUCTION_DB_HOST,
-  //     dbname: process.env.PRODUCTION_DB_NAME,
-  //     dialect: process.env.PRODUCTION_DB_DIALECT
-  //   };
-  // }
+  } else if (env === "production") {
+    return {
+      database: process.env.DB_PRODUCTION_URL,
+      username: process.env.PRODUCTION_DB_USERNAME,
+      password: process.env.PRODUCTION_DB_PASSWORD,
+      host: process.env.PRODUCTION_DB_HOST,
+      dbname: process.env.PRODUCTION_DB_NAME,
+      dialect: process.env.PRODUCTION_DB_DIALECT
+    };
+  }
 };
 module.exports = {
   DB_CONFIG: dbConfig(process.env.NODE_ENV)
