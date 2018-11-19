b1135cafa650b2ba9ae2fabaaa745c6f6b254ca8
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-19 22:19:33 +0200

    make the function of adding workers

--- src/server/controllers/workers.js
+++ src/server/controllers/workers.js
@@ -13,14 +13,14 @@ exports.get = async (req, res) => {
 exports.delete = (req, res) => {
   try {
     workers.destroy({ where: { id: req.body.workerId } }).then(() => {
-      res.status(200).send({ message: 'delete done' });
+      res.status(200).send({ message: "delete done" });
     });
   } catch (err) {
     res.status(500).send({ err });
   }
 };
 
-exports.search= async (req, res) => {
+exports.search = async (req, res) => {
   try {
     const { workerName } = req.body;
     const resultSearch = await workers.findAll({
@@ -38,24 +38,41 @@ exports.search= async (req, res) => {
   } catch (error) {
     res.send({ error });
   }
-}; 
+};
 
-exports.getDetails= async (req, res) => {
+exports.getDetails = async (req, res) => {
   try {
     const workerId = req.params.id;
     const result = await workers.findAll({
       where: {
-        id: workerId 
+        id: workerId
       }
     });
     if (result[0]) {
-      const details= (result[0].dataValues);
+      const details = result[0].dataValues;
       res.status(200).send(details);
     } else {
       res.status(404).send("Error in finding result");
     }
   } catch (error) {
-    console.log(error)
-    res.status(500).send('Server Error');
+    console.log(error);
+    res.status(500).send("Server Error");
   }
-}; 
+};
+
+exports.post = async (req, res) => {
+  try {
+    const { managerData } = req.body;
+    const { count } = await workers.findAndCountAll({
+      where: {
+        username: managerData.username
+      }
+    });
+    if (count !== 0) throw new TypeError("The name is used before");
+    await workers.create(managerData);
+    res.send({ message: "Adding worker done" });
+  } catch (error) {
+    const { message } = error;
+    res.send({ error: message });
+  }
+};
