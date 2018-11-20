ab8ac5e078e050d2c4c931fec230ebb420faed78
asalaKM <asalamatarya@gmail.com>
2018-11-20 12:03:14 +0200

    add new routs

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -22,6 +22,9 @@ Router.put('/participant/:id', participants.update);
 Router.get('/participant/:id/dates', participants.getDates);
 Router.delete('/date', participants.deleteDate);
 
+Router.get('/participant/:id/courses', participants.getCourses);
+Router.delete('/course', participants.deleteCourse);
+
 Router.get('/courses', courses.get);
 Router.delete("/courses", courses.delete);
 Router.post("/courses/search", courses.search);
