56dacb43c5111ff9bd386478340e519950120055
asalaKM <asalamatarya@gmail.com>
2018-11-20 18:06:56 +0200

    solve eslint problems

--- src/server/database/models/dates.js
+++ src/server/database/models/dates.js
@@ -2,17 +2,17 @@ const Sequelize = require('sequelize');
 const sequelize = require('../config/connection');
 
 const dates = sequelize.define('dates', {
-    date_start: {
-      type: Sequelize.DATE,
-      allowNull: false
-    },
-    date_end: {
-      type: Sequelize.DATE,
-      allowNull: false
-    },
-    note: {
-      type: Sequelize.TEXT
-    }
+  date_start: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+  date_end: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+  note: {
+    type: Sequelize.TEXT,
+  },
 });
 
 module.exports = dates;
