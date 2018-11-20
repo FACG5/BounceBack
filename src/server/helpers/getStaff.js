6af2f908b23ef231bc985f3906a7e83586d9a58b
asalaKM <asalamatarya@gmail.com>
2018-11-20 18:05:25 +0200

    solve eslint problems

--- src/server/helpers/getStaff.js
+++ src/server/helpers/getStaff.js
@@ -1,9 +1,9 @@
-const Staffs = require("../database/models/staff");
+const Staffs = require('../database/models/staff');
 
-module.exports = async username => {
+module.exports = async (username) => {
   const staff = await Staffs.findOne({
     where: { username },
-    attributes: ["role", "username", "password"]
+    attributes: ['role', 'username', 'password'],
   });
   return staff.dataValues;
 };
