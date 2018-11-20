2b615c3b0dc08c2bb757bfaca06ce1af5a1d062c
asalaKM <asalamatarya@gmail.com>
2018-11-20 18:36:46 +0200

    solve eslint problems

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const getCount = require('./getCount');
 const participants = require('./participants');
 const courses = require('./courses');
-const Login = require("./login");
-const workers = require("./workers");
-const managers = require("./managers");
+const Login = require('./login');
+const workers = require('./workers');
+const managers = require('./managers');
 
 const Router = express.Router();
 
@@ -27,24 +27,24 @@ Router.get('/participant/:id/courses', participants.getCourses);
 Router.delete('/course', participants.deleteCourse);
 
 Router.get('/courses', courses.get);
-Router.delete("/courses", courses.delete);
-Router.post("/courses/search", courses.search);
-Router.get("/course/:id", courses.details);
+Router.delete('/courses', courses.delete);
+Router.post('/courses/search', courses.search);
+Router.get('/course/:id', courses.details);
 Router.post('/courses', courses.post);
 
-Router.get("/workers", workers.get);
-Router.delete("/workers", workers.delete);
-Router.post("/workers/search", workers.search);
-Router.get("/worker/:id", workers.getDetails);
+Router.get('/workers', workers.get);
+Router.delete('/workers', workers.delete);
+Router.post('/workers/search', workers.search);
+Router.get('/worker/:id', workers.getDetails);
 Router.post('/workers', workers.post);
 
 
-Router.get("/managers", managers.get);
-Router.delete("/managers", managers.delete);
+Router.get('/managers', managers.get);
+Router.delete('/managers', managers.delete);
 Router.post('/managers/search', managers.search);
 Router.get('/manager/:id', managers.getDetails);
 Router.post('/managers', managers.post);
 
-Router.post("/login", Login.get);
+Router.post('/login', Login.get);
 
 module.exports = Router;
