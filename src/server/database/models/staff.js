68409cc5e70a952b28f6cc942d16ad8bfe64b964
asalaKM <asalamatarya@gmail.com>
2018-11-20 18:09:12 +0200

    solve eslint problems

--- src/server/database/models/staff.js
+++ src/server/database/models/staff.js
@@ -2,55 +2,55 @@ const Sequelize = require('sequelize');
 const sequelize = require('../config/connection');
 
 const staff = sequelize.define('staff', {
-    role: {
-      type: Sequelize.TEXT,
-      allowNull: false,
-      defaultValue: 'manager'
-    },
-    surname: {
-      type: Sequelize.TEXT,
-      allowNull: false
-    },
-    forename: {
-      type: Sequelize.TEXT,
-      allowNull: false
-    },
-    username: {
-      type: Sequelize.TEXT,
-      allowNull: false
-    },
-    date_of_birth: {
-      type: Sequelize.DATE,
-      allowNull: false
-    },
-    gender: {
-      type: Sequelize.TEXT,
-      allowNull: false
-    },
-    martial_status: {
-      type: Sequelize.TEXT
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    full_address: {
-      type: Sequelize.TEXT
-    },
-    postcode: {
-      type: Sequelize.TEXT
-    },
-    mobile: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    landline: {
-      type: Sequelize.STRING
-    },
-    password: {
-      type: Sequelize.TEXT,
-      allowNull: false
-    }
+  role: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    defaultValue: 'manager',
+  },
+  surname: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  forename: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  username: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  date_of_birth: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+  gender: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  martial_status: {
+    type: Sequelize.TEXT,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  full_address: {
+    type: Sequelize.TEXT,
+  },
+  postcode: {
+    type: Sequelize.TEXT,
+  },
+  mobile: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  landline: {
+    type: Sequelize.STRING,
+  },
+  password: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
 });
 
 module.exports = staff;
