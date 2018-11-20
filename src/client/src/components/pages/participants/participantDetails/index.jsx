c056460740cd22ca547a90c35350d4e652f39195
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-20 10:58:44 +0200

    make the request for update participants

--- src/client/src/components/pages/participants/participantDetails/index.jsx
+++ src/client/src/components/pages/participants/participantDetails/index.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
 import {
   state as initialState,
-  fields as fieldSet,
-  validationForm
+  fields as fieldSet
+  // validationForm
 } from "./staticData";
 import Button from "./../../../abstract/button";
 import Form from "./../../../abstract/Form";
-import Footer from '../../../abstract/footer';
+import Footer from "../../../abstract/footer";
 import axios from "axios";
-import contextHoc from './../../../abstract/HOC/contextHoc';
-import './style.css';
+import contextHoc from "./../../../abstract/HOC/contextHoc";
+import "./style.css";
+import swal from "sweetalert2";
 
 class index extends Component {
   state = initialState;
-  
+
   goBack = event => {
     this.props.history.push("/participants/view");
   };
@@ -31,31 +32,70 @@ class index extends Component {
   getDetails = async () => {
     const id = this.props.match.params.id;
     const { dispatch } = this.props.context;
-    axios(`/api/v2/participant/${id}`).then(result => {
-      const { data } = result;
-      const date = data.date_of_birth.split("T")[0];
-      this.setState({ ...data, date_of_birth: date });
-    }).catch(error => {
-      dispatch({ type: 'ERROR_PAGE', payload: { ErrorPage: error.response.status } })
-    })
+    axios(`/api/v2/participant/${id}`)
+      .then(result => {
+        const { data } = result;
+        const date = data.date_of_birth.split("T")[0];
+        this.setState({ ...data, date_of_birth: date });
+      })
+      .catch(error => {
+        dispatch({
+          type: "ERROR_PAGE",
+          payload: { ErrorPage: error.response.status }
+        });
+      });
+  };
 
+  updateParticipant = async obj => {
+    const confirm = await swal({
+      type: "warning",
+      html: "Are you sure that you want to update this data ?",
+      showCancelButton: true,
+      focusConfirm: false,
+      confirmButtonText: '<i class="fa fa-thumbs-up"></i> Yes',
+      confirmButtonAriaLabel: "Thumbs up",
+      cancelButtonText: '<i class="fa fa-thumbs-down"></i> No ',
+      cancelButtonAriaLabel: "Thumbs down"
+    });
+    if (confirm.value) {
+      const { id } = this.props.match.params;
+      const result = await axios(`/api/v2/participant/${id}`, {
+        method: "PUT",
+        data: {
+          participantData: obj
+        }
+      });
+      if (result.data.error) {
+        await swal({
+          title: "",
+          type: "warning",
+          html: result.data.error,
+          confirmButtonText: "Ok"
+        });
+        this.props.history.push("/participants/view");
+      } else {
+        await swal({
+          title: "Success",
+          type: "success",
+          html: result.data.message
+        });
+        this.setState({ ...obj });
+        this.props.history.push("/participants/view");
+      }
+    }
   };
 
   componentDidMount = () => {
     this.getDetails();
-  }
+  };
 
   // the implemention waiting  back end api
   onSubmit = event => {
     event.preventDefault();
     const fields = { ...this.state };
-    const error = validationForm(fields);
-    if (error) return this.setState({ error });
-
-    for (const key in fields) {
-      fields[key] = "";
-    }
-    this.setState(fields);
+    // const error = validationForm(fields);
+    // if (error) return this.setState({ error });
+    this.updateParticipant(fields);
   };
 
   onChange = event => {
@@ -64,7 +104,6 @@ class index extends Component {
   };
 
   render() {
-
     return (
       <>
         <Form
@@ -75,8 +114,12 @@ class index extends Component {
           btnEvents={[this.onSubmit, this.goBack]}
         />
         <div className="button-taps">
-          <Button value="Dates" onClick={this.goDates} color="#272727"/>
-          <Button value="Trainings" onClick={this.goTrainings} color="#272727"/>
+          <Button value="Dates" onClick={this.goDates} color="#272727" />
+          <Button
+            value="Trainings"
+            onClick={this.goTrainings}
+            color="#272727"
+          />
         </div>
         <Footer />
       </>
