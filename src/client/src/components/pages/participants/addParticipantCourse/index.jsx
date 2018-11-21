45dffce5d325e3ad4dce03e1ac64b88578c885ed
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-21 19:35:05 +0200

    make request for courses names

--- src/client/src/components/pages/participants/addParticipantCourse/index.jsx
+++ src/client/src/components/pages/participants/addParticipantCourse/index.jsx
@@ -6,6 +6,7 @@ import {
 } from "./staticData";
 import Form from "./../../../abstract/Form";
 import Footer from '../../../abstract/footer';
+import axios from 'axios';
 
 export default class index extends Component {
   state = initialState;
@@ -19,6 +20,22 @@ export default class index extends Component {
     const { id } = this.props.match.params;
     this.props.history.push(`/participants/${id}/courses`);
   };
+
+  getCoursesNames = async () => {
+    const data = await axios("/api/v2/courses");
+    console.log(data);
+    const final = data.data.coursesData;
+    let coursesName = fieldSet[0][0].options;
+    final.map(value => {
+      return coursesName.push(value.course_name);
+    });
+
+    this.setState({ coursesName: final[0].course_name });
+  };
+
+  componentWillMount = () => {
+    this.getCoursesNames();
+  }
 
   // the implemention waiting  back end api
   onSubmit = event => {
