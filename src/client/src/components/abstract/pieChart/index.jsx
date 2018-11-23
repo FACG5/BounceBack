7233287458b9329e738f23873eab50ee571de2c5
Kanana <knanahassouna2@gmail.com>
2018-11-23 11:18:11 +0200

    using react Ref

--- src/client/src/components/abstract/pieChart/index.jsx
+++ src/client/src/components/abstract/pieChart/index.jsx
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 import { makePie, colors } from "./logic";
 import "./index.css";
 export default class index extends Component {
+  constructor(props) {
+    super(props);
+    this.chartTag = React.createRef();
+  }
   componentDidMount = () => {
-    const { sections, id } = this.props;
-    makePie(sections, id);
+    const { sections } = this.props;
+    makePie(sections, this.chartTag.current);
   };
   render() {
     const { sections, id } = this.props;
     return (
       <div className="pie">
-        <svg className={`bar-chart-${id} bar-chart`} />
+        <svg className={`bar-chart-${id} bar-chart`} ref={this.chartTag} />
         <div className="platforms">
           {sections.map((section, i) => (
             <div className="paltform-titles" key={i}>
