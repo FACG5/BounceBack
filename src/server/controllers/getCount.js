6037c5199797c4eaed5f7ac944078758e313c07a
asalaKM <asalamatarya@gmail.com>
2018-11-23 14:40:22 +0200

    edit git count function to git the participnt counts depend on employment status

--- src/server/controllers/getCount.js
+++ src/server/controllers/getCount.js
@@ -1,13 +1,38 @@
+const { Op } = require('sequelize');
+
 const participant = require('../database/models/participant');
 const course = require('../database/models/course');
 const worker = require('../database/models/worker');
+
 
 exports.get = async (req, res) => {
   try {
     const countParticipant = await participant.findAndCountAll();
     const countCourse = await course.findAndCountAll();
     const countWorker = await worker.findAndCountAll();
-    res.send({ countParticipant, countCourse, countWorker });
+    const notEmployed = 'not employed yet';
+    const employed = 'employed';
+    const countEmployedParticipant = await participant.findAndCountAll({
+      where: {
+        employment_outcomes: {
+          [Op.like]: `${employed}`,
+        },
+      },
+    });
+    const countNotEmployedParticipant = await participant.findAndCountAll({
+      where: {
+        employment_outcomes: {
+          [Op.like]: `${notEmployed}`,
+        },
+      },
+    });
+    res.send({
+      countParticipant,
+      countCourse,
+      countWorker,
+      countEmployedParticipant,
+      countNotEmployedParticipant,
+    });
   } catch (err) {
     res.status(500).send({ err });
   }
