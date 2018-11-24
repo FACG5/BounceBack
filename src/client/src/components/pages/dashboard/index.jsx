7edeeba4dfe9563ed3ff7c2fec2e44fd1d0c4cf4
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-24 12:15:04 +0200

    edit the title of piechart

--- src/client/src/components/pages/dashboard/index.jsx
+++ src/client/src/components/pages/dashboard/index.jsx
@@ -60,11 +60,11 @@ class index extends Component {
         worker: countWorker,
         employment: [
           { title: "Employed", percentage: employedAvg },
-          { title: "Un_employed", percentage: notEmployedAvg }
+          { title: "Unemployed", percentage: notEmployedAvg }
         ],
         offending: [
           { title: "Bounce Back", percentage: notOffendingAvg },
-          { title: "Othder", percentage: offendingAvg }
+          { title: "Other", percentage: offendingAvg }
         ],
         loading: false
       });
