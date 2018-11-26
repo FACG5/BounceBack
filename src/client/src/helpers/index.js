5f035ebb0f3dd35bcf5bca0c654386cbeeff831d
Kanana <knanahassouna2@gmail.com>
2018-11-26 19:59:58 +0200

    helper function to unlock cookie and get user object

---
+++ src/client/src/helpers/index.js
@@ -0,0 +1,15 @@
+
+import { decode } from 'jsonwebtoken';
+
+export function checkUser() {
+  const token = getCookie('jwt');
+  console.log(token)
+  return decode(token);
+}
+
+
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+} 
