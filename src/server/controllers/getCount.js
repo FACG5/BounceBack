cd88b44cb406458dc14d1c59c6694b4b0604ce44
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-11-26 13:59:08 +0200

    make the query of get employments data

--- src/server/controllers/getCount.js
+++ src/server/controllers/getCount.js
@@ -1,6 +1,7 @@
 const participant = require('../database/models/participant');
 const course = require('../database/models/course');
 const worker = require('../database/models/worker');
+const sequelize = require('./../database/config/connection');
 
 exports.get = async (req, res) => {
   try {
@@ -19,12 +20,15 @@ exports.get = async (req, res) => {
         reoffending: offending,
       },
     });
+    const counts = await sequelize
+      .query("SELECT participant_courses.course_name,count(*) FROM participants JOIN participant_courses ON participant_courses.participant_id = participants.id where participants.employment_outcomes = 'employed' group by (participant_courses.course_name)");
     res.send({
       countParticipant,
       countCourse,
       countWorker,
       countEmployedParticipant,
       countOffending,
+      counts,
     });
   } catch (err) {
     res.status(500).send({ err });
