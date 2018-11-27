13cd652aa10e73567fba77f6bc947a1807df3fb3
Kanana <knanahassouna2@gmail.com>
2018-11-27 14:10:35 +0200

    all Compoenets Imports

---
+++ src/client/src/components/Routes/allComponents.js
@@ -0,0 +1,54 @@
+import Sidebar from "../abstract/sidebar";
+import Container from "../abstract/layout/Container";
+import ViewParticipants from "../pages/participants/viewParticipants";
+import viewCourses from "../pages/courses/viewCourses";
+import viewManagers from "../pages/managers/viewManagers";
+import addManager from "../pages/managers/addManager";
+import participantDate from "../pages/participants/participantDates";
+import participantCourse from "../pages/participants/participantCourses";
+import Dashboard from "../pages/dashboard";
+import AddParticipant from "../pages/participants/addParticipant";
+import ViewWorker from "../pages/workers/viewWorkers";
+import addCourse from "../pages/courses/addCourses";
+import ReportResult from "../pages/reportResult";
+import Report from "../pages/report";
+import prisonDetails from "../pages/participants/prisonDetails";
+import addWorker from "../pages/workers/addWorkers";
+import workerDetails from "../pages/workers/workerDetails";
+import courseDetails from "../pages/courses/courseDetails";
+import pastoral from "../pages/courses/pastoral";
+import managerDetails from "../pages/managers/managerDetails";
+import ParticipantCourseDetails from "../pages/participants/participantCourseDetails";
+import ParticipantCourseAdd from "../pages/participants/addParticipantCourse";
+import AddParticipantDate from "../pages/participants/addParticipantDate";
+import ParticipantDateDetails from "../pages/participants/participantDateDetails";
+import participantDetails from "../pages/participants/participantDetails";
+
+export default {
+  Sidebar,
+  Container,
+  ViewParticipants,
+  viewCourses,
+  viewManagers,
+  addManager,
+  participantDate,
+  participantCourse,
+  Dashboard,
+  AddParticipant,
+  ViewWorker,
+  addCourse,
+  ReportResult,
+  Report,
+  prisonDetails,
+  addWorker,
+  workerDetails,
+  courseDetails,
+  pastoral,
+  managerDetails,
+  ParticipantCourseDetails,
+  ParticipantCourseAdd,
+  AddParticipantDate,
+  ParticipantDateDetails,
+  participantDetails
+
+}
