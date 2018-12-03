e88fcde0d82ce43d38e12f53f817f3774ba2153a
Kanana <knanahassouna2@gmail.com>
2018-12-03 10:27:24 +0200

    add validation functions

--- src/client/src/helpers/index.js
+++ src/client/src/helpers/index.js
@@ -9,4 +9,20 @@ function getCookie(name) {
   var value = "; " + document.cookie;
   var parts = value.split("; " + name + "=");
   if (parts.length === 2) return parts.pop().split(";").shift();
-} 
+}
+
+export function getDate() {
+  const date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  if (day < 10) day = '0' + day;
+  if (month < 10) month = '0' + month;
+  return `${year}-${month}-${day}`;
+}
+
+
+export function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
