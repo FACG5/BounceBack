462b708f68bad668fc0aa8f54a71574c532afab9
Kanana <knanahassouna2@gmail.com>
2018-12-03 10:55:24 +0200

    remove email validation function

--- src/client/src/helpers/index.js
+++ src/client/src/helpers/index.js
@@ -20,9 +20,3 @@ export function getDate() {
   if (month < 10) month = '0' + month;
   return `${year}-${month}-${day}`;
 }
-
-
-export function validateEmail(email) {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
