8c2662e9e4556ae1ebd290243b0eca298f4234cb
asalaKM <asalamatarya@gmail.com>
2018-12-04 12:27:51 +0200

    add prisoner details that can view the details edit it

---
+++ src/client/src/components/pages/participants/prisonerDetails/index.jsx
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import {
+  state as initialState,
+  fields as fieldSet,
+  validationForm
+} from "./staticData";
+import Form from "./../../../abstract/Form";
+import Footer from '../../../abstract/footer';
+import axios from "axios";
+import swal from "sweetalert2";
+import contextHoc from './../../../abstract/HOC/contextHoc';
+
+class PresionDetails extends Component {
+  state = initialState;
+
+  onChange = event => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  };
+  goBack = event => {
+    this.props.history.push('/participants/add')
+  };
+
+  getPrisonerDetails = async () => {
+    const id = this.props.match.params.id;
+    const { dispatch } = this.props.context;  
+    axios(`/api/v2/participants/${id}/prison`)
+      .then( async result => {
+        const data = result.data.getPrisoner.rows[0];
+        this.setState({ ...data, loading: false })
+      })
+      .catch(error => {
+        dispatch({
+          type: "ERROR_PAGE",
+          payload: { ErrorPage: error.response.status }
+        });
+      })
+  };
+  componentDidMount = () => {
+    this.getPrisonerDetails();
+  };
+
+  updatePrisoner = async data => {
+    const confirm = await swal({
+      type: "warning",
+      html: "Are you sure that you want to update this data ?",
+      showCancelButton: true,
+      focusConfirm: false,
+      confirmButtonText: '<i class="fa fa-thumbs-up"></i> Yes',
+      confirmButtonAriaLabel: "Thumbs up",
+      cancelButtonText: '<i class="fa fa-thumbs-down"></i> No ',
+      cancelButtonAriaLabel: "Thumbs down"
+    });
+    if (confirm.value) {
+      const prisonId = this.props.match.params.prisonerId;
+      const result = await axios(`/api/v2/prison/${prisonId}`, {
+        method: "PUT",
+        data: {
+           prisonerData: data
+        }
+      });
+      if (result.data.error) {
+        await swal({
+          title: "",
+          type: "warning",
+          html: result.data.error,
+          confirmButtonText: "Ok"
+        });
+        this.props.history.push(`/`);
+      } else {
+        await swal({
+          title: "Success",
+          type: "success",
+          html: result.data.message
+        });
+        this.setState({ ...data });
+        this.props.history.push("/participants/view");
+      }
+    }
+  };
+
+  // Edit Data
+  onSubmit = event => {
+    event.preventDefault();
+    const fields = { ...this.state };
+    this.updatePrisoner(fields);
+  };
+
+  render() {
+    return (
+      <div>
+        <Form
+          title="Prisoner Details"
+          fields={fieldSet}
+          values={this.state}
+          onChange={this.onChange}
+          btnEvents={[this.onSubmit, this.goBack]}
+        />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default contextHoc(PresionDetails);
---
+++ src/client/src/components/pages/participants/prisonerDetails/staticData.js
@@ -0,0 +1,103 @@
+export const state = {
+    utr_number: "",
+    prison_name: "",
+    prison_number: "",
+    officer_name: "",
+    prison_in: "",
+    prison_release: "",
+    rotl_hdc: "",
+    offence: "",
+    point_of_contact: ""
+  };
+  export const fields = [
+      [{
+              tag: "Input",
+              label: "UTR. No.",
+              name: "utr_number",
+              width: "15rem",
+              placeholder: "UTR. No"
+          },
+          {
+              tag: "Input",
+              label: "Prison",
+              name: "prison_name",
+              width: "15rem",
+              placeholder: "prison"
+  
+          },
+          {
+            tag: "Input",
+            label: "Prison No",
+            name: "prison_number",
+            width: "15rem",
+            placeholder: "prison number"
+    
+        }
+      ],
+      [{
+          tag: "Input",
+          label: "Probation officer name",
+          name: "officer_name",
+          width: "35rem",
+          placeholder: "probation officer name"
+  
+      }],
+      
+      [{
+              tag: "Input",
+              label: "Length of Sentence",
+              name: "prison_in",
+              width: "15rem",
+              type: "number",
+  
+          },
+          {
+              tag: "Input",
+              label: "Prison Release",
+              name: "prison_release",
+              width: "15rem",
+              type: "date",
+          },
+          {
+              tag: "Input",
+              label: "ROTL/HDC",
+              name: "rotl_hdc",
+              width: "15rem",
+              placeholder: "ROTL / HDC"
+          }
+      ],
+      [{
+          tag: "Input",
+          label: "Offence",
+          name: "offence",
+          width: "20rem",
+          placeholder: "offence"
+  
+      },
+      {
+        tag: "Input",
+        label: "Prison Point Of Contact",
+        name: "point_of_contact",
+        width: "20rem",
+        placeholder: "prison point of contact"
+
+    }],
+      [{
+          tag: 'Button',
+          value: "Edit & Save",
+          color: "#272727",
+          staticField: true
+      }, {
+          tag: 'Button',
+          value: "Back",
+          color: "#FF4800",
+          staticField: true
+      }, ]
+  ];
+  
+  export const validationForm = fields => {
+    for (const key in fields) {
+      if (fields[key] === "") return `Please Check ${key}`;
+    }
+  };
+  
