12e4dbc14e8594d7573e13a30c7e8e19e4ec8710
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-12-09 14:51:12 +0200

    make the route for function

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -23,7 +23,8 @@ Router.get('/overview', getCount.get);
 // Participants Routes
 Router.get('/participants', participants.get);
 Router.get('/participant/:id', participants.getDetails);
-Router.post('/participants/search/name', participants.searchByName);
+Router.post('/participants/search/forename', participants.searchByForeName);
+Router.post('/participants/search/surname', participants.searchBySurName);
 Router.post('/participants/search/date', participants.searchBydate);
 Router.post('/participants', participants.post);
 Router.put('/participant/:id', participants.update);
