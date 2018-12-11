d8a8d9249bdeb140ac794e39d7f3f21d1bc21a2d
asalaKM <asalamatarya@gmail.com>
2018-12-11 09:47:58 +0200

    remove link component

--- src/client/src/components/abstract/sidebar/sidebarLinks/index.jsx
+++ src/client/src/components/abstract/sidebar/sidebarLinks/index.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import swal from 'sweetalert2';
 import { Consumer } from '../../../context';
 import removeCookies from './removeCookies';
