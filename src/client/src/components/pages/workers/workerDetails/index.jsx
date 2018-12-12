90e8090547066fd2b8a42be3d27f80313228f162
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-12-12 10:15:31 +0200

    make validation for update staff details

--- src/client/src/components/pages/workers/workerDetails/index.jsx
+++ src/client/src/components/pages/workers/workerDetails/index.jsx
@@ -6,6 +6,7 @@ import propTypes from 'prop-types';
 import {
   state as initialState,
   fields as fieldSet,
+  validationForm,
 } from './staticData';
 import Form from '../../../abstract/Form';
 import Footer from '../../../abstract/footer';
@@ -83,6 +84,8 @@ class index extends Component {
   onSubmit = (event) => {
     event.preventDefault();
     const fields = { ...this.state };
+    const error = validationForm(fields);
+    if (error) return this.setState({ error });
     this.updateWorker(fields);
   };
 
